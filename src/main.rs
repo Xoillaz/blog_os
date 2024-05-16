6082ceddc3020ae910a2fce5125a589b0f12b347
Xoillaz <xorillaz.persona@gmail.com>
2024-05-16 23:37:33 +0800

    init: Implement minimal kernel using bootimage

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,26 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 // Don't mangle the function.
 #[no_mangle]
 // This function is the entry point, since the link$
 // er looks for a function named `_start` by default
 pub extern "C" fn _start() -> ! {
+    // Cast the integer into a raw pointer.
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // Use offset() to write the string byte and color byte.
+        //
+        // Tell the compiler that the operations are valid.
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
